shuangshuangwang/spark-adaptive

Enable stats estimation for physical plan

sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/QueryStageInput.scala
@@ -23,6 +23,7 @@ import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeMap, Expression, SortOrder}
 import org.apache.spark.sql.catalyst.plans.physical.{HashPartitioning, Partitioning, UnknownPartitioning}
 import org.apache.spark.sql.execution._
+import org.apache.spark.sql.execution.statsEstimation.Statistics
 
 /**
  * QueryStageInput is the leaf node of a QueryStage and is used to hide its child stage. It gets
@@ -53,6 +54,10 @@ abstract class QueryStageInput extends LeafExecNode {
 
   override def outputOrdering: Seq[SortOrder] = {
     childStage.outputOrdering.map(updateAttr(_).asInstanceOf[SortOrder])
+  }
+
+  override def computeStats(): Statistics = {
+    childStage.stats
   }
 
   override def generateTreeString(

sql/core/src/main/scala/org/apache/spark/sql/execution/exchange/Exchange.scala
@@ -27,6 +27,7 @@ import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeMap, Expre
 import org.apache.spark.sql.catalyst.plans.physical.{HashPartitioning, Partitioning}
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.execution.{LeafExecNode, SparkPlan, UnaryExecNode}
+import org.apache.spark.sql.execution.statsEstimation.Statistics
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.StructType
 
@@ -76,6 +77,10 @@ case class ReusedExchangeExec(override val output: Seq[Attribute], child: Exchan
 
   override def outputOrdering: Seq[SortOrder] = {
     child.outputOrdering.map(updateAttr(_).asInstanceOf[SortOrder])
+  }
+
+  override def computeStats(): Statistics = {
+    child.stats
   }
 }
 

sql/core/src/main/scala/org/apache/spark/sql/execution/statsEstimation/SizeInBytesOnlyStatsPlanVisitor.scala
@@ -1,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.statsEstimation
+
+import org.apache.spark.sql.catalyst.plans.{LeftAnti, LeftSemi}
+import org.apache.spark.sql.execution._
+import org.apache.spark.sql.execution.aggregate._
+import org.apache.spark.sql.execution.exchange.ShuffleExchangeExec
+import org.apache.spark.sql.execution.joins.{HashJoin, SortMergeJoinExec}
+
+object SizeInBytesOnlyStatsPlanVisitor extends SparkPlanVisitor[Statistics] {
+
+  private def visitUnaryExecNode(p: UnaryExecNode): Statistics = {
+    // There should be some overhead in Row object, the size should not be zero when there is
+    // no columns, this help to prevent divide-by-zero error.
+    val childRowSize = p.child.output.map(_.dataType.defaultSize).sum + 8
+    val outputRowSize = p.output.map(_.dataType.defaultSize).sum + 8
+    // Assume there will be the same number of rows as child has.
+    var sizeInBytes = (p.child.stats.sizeInBytes * outputRowSize) / childRowSize
+    if (sizeInBytes == 0) {
+      // sizeInBytes can't be zero, or sizeInBytes of BinaryNode will also be zero
+      // (product of children).
+      sizeInBytes = 1
+    }
+
+    // Don't propagate rowCount and attributeStats, since they are not estimated here.
+    Statistics(sizeInBytes = sizeInBytes)
+  }
+
+  override def default(p: SparkPlan): Statistics = p match {
+    case p: LeafExecNode => p.computeStats()
+    case _: SparkPlan => Statistics(sizeInBytes = p.children.map(_.stats.sizeInBytes).product)
+  }
+
+  override def visitFilterExec(p: FilterExec): Statistics = visitUnaryExecNode(p)
+
+  override def visitProjectExec(p: ProjectExec): Statistics = visitUnaryExecNode(p)
+
+  override def visitHashAggregateExec(p: HashAggregateExec): Statistics = {
+    if (p.groupingExpressions.isEmpty) {
+      val sizeInBytes = 8 + p.output.map(_.dataType.defaultSize).sum
+      Statistics(sizeInBytes)
+    } else {
+      visitUnaryExecNode(p)
+    }
+  }
+
+  override def visitHashJoin(p: HashJoin): Statistics = {
+    p.joinType match {
+      case LeftAnti | LeftSemi =>
+        // LeftSemi and LeftAnti won't ever be bigger than left
+        p.left.stats
+      case _ =>
+        Statistics(sizeInBytes = p.left.stats.sizeInBytes * p.right.stats.sizeInBytes)
+    }
+  }
+
+  override def visitShuffleExchangeExec(p: ShuffleExchangeExec): Statistics = {
+    if (p.mapOutputStatistics != null) {
+      val sizeInBytes = p.mapOutputStatistics.bytesByPartitionId.sum
+      val bytesByPartitionId = p.mapOutputStatistics.bytesByPartitionId
+      Statistics(sizeInBytes = sizeInBytes, bytesByPartitionId = Some(bytesByPartitionId))
+    } else {
+      visitUnaryExecNode(p)
+    }
+  }
+
+  override def visitSortAggregateExec(p: SortAggregateExec): Statistics = {
+    if (p.groupingExpressions.isEmpty) {
+      val sizeInBytes = 8 + p.output.map(_.dataType.defaultSize).sum
+      Statistics(sizeInBytes)
+    } else {
+      visitUnaryExecNode(p)
+    }
+  }
+
+  override def visitSortMergeJoinExec(p: SortMergeJoinExec): Statistics = {
+    p.joinType match {
+      case LeftAnti | LeftSemi =>
+        // LeftSemi and LeftAnti won't ever be bigger than left
+        p.left.stats
+      case _ =>
+        default(p)
+    }
+  }
+}

sql/core/src/main/scala/org/apache/spark/sql/execution/statsEstimation/SparkPlanStats.scala
@@ -1,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.statsEstimation
+
+import org.apache.spark.sql.execution.SparkPlan
+
+/**
+ * A trait to add statistics propagation to [[SparkPlan]].
+ */
+trait SparkPlanStats { self: SparkPlan =>
+
+  /**
+   * Returns the estimated statistics for the current logical plan node. Under the hood, this
+   * method caches the return value, which is computed based on the configuration passed in the
+   * first time. If the configuration changes, the cache can be invalidated by calling
+   * [[invalidateStatsCache()]].
+   */
+  def stats: Statistics = statsCache.getOrElse {
+    statsCache = Option(SizeInBytesOnlyStatsPlanVisitor.visit(self))
+    statsCache.get
+  }
+
+  /** A cache for the estimated statistics, such that it will only be computed once. */
+  protected var statsCache: Option[Statistics] = None
+
+  /** Invalidates the stats cache. See [[stats]] for more information. */
+  final def invalidateStatsCache(): Unit = {
+    statsCache = None
+    children.foreach(_.invalidateStatsCache())
+  }
+}

luzhonghao/spark

sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/QueryStageInput.scala
@@ -23,6 +23,7 @@ import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeMap, Expression, SortOrder}
 import org.apache.spark.sql.catalyst.plans.physical.{HashPartitioning, Partitioning, UnknownPartitioning}
 import org.apache.spark.sql.execution._
+import org.apache.spark.sql.execution.statsEstimation.Statistics
 
 /**
  * QueryStageInput is the leaf node of a QueryStage and is used to hide its child stage. It gets
@@ -53,6 +54,10 @@ abstract class QueryStageInput extends LeafExecNode {
 
   override def outputOrdering: Seq[SortOrder] = {
     childStage.outputOrdering.map(updateAttr(_).asInstanceOf[SortOrder])
+  }
+
+  override def computeStats(): Statistics = {
+    childStage.statsPlan
   }
 
   override def generateTreeString(

sql/core/src/main/scala/org/apache/spark/sql/execution/exchange/Exchange.scala
@@ -27,6 +27,7 @@ import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeMap, Expre
 import org.apache.spark.sql.catalyst.plans.physical.{HashPartitioning, Partitioning}
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.execution.{LeafExecNode, SparkPlan, UnaryExecNode}
+import org.apache.spark.sql.execution.statsEstimation.Statistics
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.StructType
 
@@ -76,6 +77,10 @@ case class ReusedExchangeExec(override val output: Seq[Attribute], child: Exchan
 
   override def outputOrdering: Seq[SortOrder] = {
     child.outputOrdering.map(updateAttr(_).asInstanceOf[SortOrder])
+  }
+
+  override def computeStats(): Statistics = {
+    child.statsPlan
   }
 }
 

sql/core/src/main/scala/org/apache/spark/sql/execution/statsEstimation/SizeInBytesOnlyStatsPlanVisitor.scala
@@ -1,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.statsEstimation
+
+import org.apache.spark.sql.catalyst.plans.{LeftAnti, LeftSemi}
+import org.apache.spark.sql.execution._
+import org.apache.spark.sql.execution.aggregate._
+import org.apache.spark.sql.execution.exchange.ShuffleExchangeExec
+import org.apache.spark.sql.execution.joins.{HashJoin, SortMergeJoinExec}
+
+object SizeInBytesOnlyStatsPlanVisitor extends SparkPlanVisitor[Statistics] {
+
+  private def visitUnaryExecNode(p: UnaryExecNode): Statistics = {
+    // There should be some overhead in Row object, the size should not be zero when there is
+    // no columns, this help to prevent divide-by-zero error.
+    val childRowSize = p.child.output.map(_.dataType.defaultSize).sum + 8
+    val outputRowSize = p.output.map(_.dataType.defaultSize).sum + 8
+    // Assume there will be the same number of rows as child has.
+    var sizeInBytes = (p.child.statsPlan.sizeInBytes * outputRowSize) / childRowSize
+    if (sizeInBytes == 0) {
+      // sizeInBytes can't be zero, or sizeInBytes of BinaryNode will also be zero
+      // (product of children).
+      sizeInBytes = 1
+    }
+
+    // Don't propagate rowCount and attributeStats, since they are not estimated here.
+    Statistics(sizeInBytes = sizeInBytes)
+  }
+
+  override def default(p: SparkPlan): Statistics = p match {
+    case p: LeafExecNode => p.computeStats()
+    case _: SparkPlan => Statistics(sizeInBytes = p.children.map(_.statsPlan.sizeInBytes).product)
+  }
+
+  override def visitFilterExec(p: FilterExec): Statistics = visitUnaryExecNode(p)
+
+  override def visitProjectExec(p: ProjectExec): Statistics = visitUnaryExecNode(p)
+
+  override def visitHashAggregateExec(p: HashAggregateExec): Statistics = {
+    if (p.groupingExpressions.isEmpty) {
+      val sizeInBytes = 8 + p.output.map(_.dataType.defaultSize).sum
+      Statistics(sizeInBytes)
+    } else {
+      visitUnaryExecNode(p)
+    }
+  }
+
+  override def visitHashJoin(p: HashJoin): Statistics = {
+    p.joinType match {
+      case LeftAnti | LeftSemi =>
+        // LeftSemi and LeftAnti won't ever be bigger than left
+        p.left.statsPlan
+      case _ =>
+        Statistics(sizeInBytes = p.left.statsPlan.sizeInBytes * p.right.statsPlan.sizeInBytes)
+    }
+  }
+
+  override def visitShuffleExchangeExec(p: ShuffleExchangeExec): Statistics = {
+    if (p.mapOutputStatistics != null) {
+      val sizeInBytes = p.mapOutputStatistics.bytesByPartitionId.sum
+      val bytesByPartitionId = p.mapOutputStatistics.bytesByPartitionId
+      Statistics(sizeInBytes = sizeInBytes, bytesByPartitionId = Some(bytesByPartitionId))
+    } else {
+      visitUnaryExecNode(p)
+    }
+  }
+
+  override def visitSortAggregateExec(p: SortAggregateExec): Statistics = {
+    if (p.groupingExpressions.isEmpty) {
+      val sizeInBytes = 8 + p.output.map(_.dataType.defaultSize).sum
+      Statistics(sizeInBytes)
+    } else {
+      visitUnaryExecNode(p)
+    }
+  }
+
+  override def visitSortMergeJoinExec(p: SortMergeJoinExec): Statistics = {
+    p.joinType match {
+      case LeftAnti | LeftSemi =>
+        // LeftSemi and LeftAnti won't ever be bigger than left
+        p.left.statsPlan
+      case _ =>
+        default(p)
+    }
+  }
+}

sql/core/src/main/scala/org/apache/spark/sql/execution/statsEstimation/SparkPlanStats.scala
@@ -1,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.statsEstimation
+
+import org.apache.spark.sql.execution.SparkPlan
+
+/**
+ * A trait to add statistics propagation to [[SparkPlan]].
+ */
+trait SparkPlanStats { self: SparkPlan =>
+
+  /**
+   * Returns the estimated statistics for the current logical plan node. Under the hood, this
+   * method caches the return value, which is computed based on the configuration passed in the
+   * first time. If the configuration changes, the cache can be invalidated by calling
+   * [[invalidateStatsCache()]].
+   */
+  def statsPlan: Statistics = statsCache.getOrElse {
+    statsCache = Option(SizeInBytesOnlyStatsPlanVisitor.visit(self))
+    statsCache.get
+  }
+
+  /** A cache for the estimated statistics, such that it will only be computed once. */
+  protected var statsCache: Option[Statistics] = None
+
+  /** Invalidates the stats cache. See [[statsPlan]] for more information. */
+  final def invalidateStatsCache(): Unit = {
+    statsCache = None
+    children.foreach(_.invalidateStatsCache())
+  }
+}